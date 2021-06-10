112a6004b19ff3bade3077ea7ab27120904df86c
Ryan Baker <rbaker699@gmail.com>
2021-06-10 14:55:10 -0700

    Update

--- server/db/models/InvalidToken.js
+++ server/db/models/InvalidToken.js
@@ -1,6 +1,26 @@
 const index = require('../index');
+const jwt = require('jsonwebtoken');
+
 const InvalidToken = index.mongoose.model('InvalidToken', {
   token: String
 });
+
+setInterval(async () => {
+  let allTokens = await InvalidToken.find().exec();
+  let invalidTokens = [];
+  for (let token of allTokens) {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET);
+    } catch (e) {
+      invalidTokens.push(token);
+    }
+  }
+  await InvalidToken.deleteMany({
+    token: {
+      $in: invalidTokens
+    }
+  }).exec();
+}, 3600000);
+
 
 module.exports = InvalidToken;
