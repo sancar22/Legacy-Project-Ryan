dacb6af2352c0bc4f3401c7b59a7c0ca066cf8c9
Ryan Baker <rbaker699@gmail.com>
2021-06-10 14:30:22 -0700

    update

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_URI, {
+
+let url = process.env.DB_URI;
+console.log(url);
+url = url.replace('{password}', process.env.PASSWORD);
+
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('DB connected'), err => console.log(err));
